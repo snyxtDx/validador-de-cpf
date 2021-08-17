6b523f7e4819b25b8e65056c69a92f08f09dd20d
snyxt-spy <76827160+snyxt-spy@users.noreply.github.com>
2021-08-17 01:24:02 -0300

    Adicionado o arquivo principal

---
+++ cpf-validator2.js
@@ -0,0 +1,17 @@
+function cpfValidator(cpf) {
+    const cleanCpf = Array.from(cpf.replace(/\D+/g, ''));
+    const checkedCpf = [...cleanCpf];
+    let digit = 0;
+    checkedCpf.splice(-2, 2);
+
+    for(let c = 10; c <= 11; c++) {
+        digit = 11 - ((checkedCpf.map((value, indice) => Number(value) * (c -indice)).reduce((x, val) => x += val, 0)) % 11);
+        digit > 9 ? digit = 0 : digit;
+        checkedCpf.push(digit);
+    }
+
+    if(checkedCpf.join('') === cleanCpf.join('')) {return 'Cpf válido'} else {return 'Cpf inválido'}; 
+
+}
+
+console.log(cpfValidator('267.930.090-44'));
